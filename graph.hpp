b06b8c52fbda17a23cd1349cfc501f509cf715d0
SamLita <samlitalyly1706lala@gmail.com>
2024-12-28 14:36:00 +0700

    graph traversal

--- graph.hpp
+++ graph.hpp
@@ -1,1 +1,82 @@
+#ifndef GRAPH_H
+#define GRAPH_H
 
+#include <iostream>
+#include <vector>
+#include <queue>
+
+class Graph {
+private:
+    int vertices; 
+    std::vector<std::vector<int>> adjList;
+
+public:
+    
+    Graph(int v) : vertices(v) {
+        adjList.resize(v);
+    }
+
+    void addEdge(int src, int dest) {
+        adjList[src].push_back(dest);
+        adjList[dest].push_back(src); 
+    }
+
+ 
+    void displayGraph() {
+        for (int i = 0; i < vertices; ++i) {
+            std::cout << "Node " << i << ": ";
+            for (int neighbor : adjList[i]) {
+                std::cout << neighbor << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+   
+    void dfsUtil(int node, std::vector<bool>& visited) {
+        visited[node] = true;
+        std::cout << node << " ";
+
+        for (int neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                dfsUtil(neighbor, visited);
+            }
+        }
+    }
+
+    
+    void dfs(int start) {
+        std::vector<bool> visited(vertices, false);
+        std::cout << "DFS Traversal: ";
+        dfsUtil(start, visited);
+        std::cout << "\n";
+    }
+
+    
+    void bfs(int start) {
+        std::vector<bool> visited(vertices, false);
+        std::queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        std::cout << "BFS Traversal: ";
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            std::cout << node << " ";
+
+            for (int neighbor : adjList[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        std::cout << "\n";
+    }
+};
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "graph.hpp"
+
+int main() {
+    
+    Graph g(5);
+
+   
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    std::cout << "Graph Representation:\n";
+    g.displayGraph();
+    g.dfs(0);
+    g.bfs(0);
+
+    return 0;
+}
 
